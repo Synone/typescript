dd60856ae408c714d31170b22a56593ae23d7f33
Sony <syno3311@gmail.com>
2022-09-29 13:18:54 +0700

    data-structure

--- types.ts
+++ types.ts
@@ -1,3 +1,9 @@
+/**
+ * #region
+ *
+ */
+
+//  primative types
 type Primatives =
   | number
   | string
@@ -6,3 +12,23 @@ type Primatives =
   | bigint
   | undefined
   | null;
+
+const primative: Primatives = true;
+// literal types
+type Literals = 20 | 'Hello' | true | 10000n;
+/* | ... */
+
+const twenty: Literals = 20;
+
+// Data structures
+
+type DataStructures =
+  | { key1: boolean; key2: number }
+  | { [key1: string]: number }
+  | [number, boolean]
+  | number[];
+const data: DataStructures = { ['Sony']: 100 };
+console.log(data);
+// Union and intersections
+
+type Union = number | boolean;
